90c2b2e2d3cf68c89b3a6433646ecfd53283e8ac
Homayoon Farrahi <homayoonfarrahi@yahoo.com>
2017-06-09 16:26:20 -0600

    If a plane vertex goes outside the picture, its anchor points will not be shown anymore. Duplicate anchor points will be removed before processing. Handling the case when the anchor line is entirely outside the boundaries.

--- anchors.js
+++ anchors.js
@@ -20,6 +20,15 @@ function AnchorSystem(boundaries) {
     }
   }
 
+  this.isCloseToRegion = function(p, maxDistance) {
+    if (p.x >= this.minX - maxDistance && p.x <= this.maxX + maxDistance
+        && p.y >= this.minY - maxDistance && p.y <= this.maxY + maxDistance) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
   this.setPaperInstance = function(paper) {
     this.paper = paper;
   }
@@ -70,33 +79,24 @@ function AnchorLine(circle1, circle2, anchorSystem) {
       anchors.push(new Point2D(new Line(p1, p2).getXforY(this.anchorSystem.maxY), this.anchorSystem.maxY));
     }
 
-    if (anchors.length !== 2) {
-      console.error('2 anchor points should be found for each line instead of: ' + anchors.length);
+    // remove duplicate anchor points
+    if (anchors.length !== 2 && anchors.length !== 0) {
+      for (var i = 0 ; i < anchors.length ; i++) {
+        for (var j = i + 1 ; j < anchors.length ; j++) {
+          if (anchors[i].x === anchors[j].x && anchors[i].y === anchors[j].y) {
+            anchors.splice(j, 1);
+            j = i + 1;
+          }
+        }
+      }
+    }
+
+    if (anchors.length !== 2 && anchors.length !== 0) {
+      console.error('only 0 or 2 anchors are accepted, not ' + anchors.length);
     }
 
     return anchors;
   }
-
-  var p1 = new Point2D(this.circle1.circle.attr('cx'), this.circle1.circle.attr('cy'));
-  var p2 = new Point2D(this.circle2.circle.attr('cx'), this.circle2.circle.attr('cy'));
-  var anchors = this.computeAnchorPositions();
-  var deltaP = p2.clone().subtract(p1);
-  var deltaAnchor = anchors[1].clone().subtract(anchors[0]);
-  if ((deltaP.x * deltaAnchor.x >= 0) && (deltaP.y * deltaAnchor.y >= 0)) {
-    this.anchorPoint1 = new AnchorPoint(anchors[0], this.circle1, this);
-    this.anchorPoint2 = new AnchorPoint(anchors[1], this.circle2, this);
-  } else {
-    this.anchorPoint1 = new AnchorPoint(anchors[1], this.circle1, this);
-    this.anchorPoint2 = new AnchorPoint(anchors[0], this.circle2, this);
-  }
-
-  this.anchorPoint1.setPaperInstance(this.paper);
-  this.anchorPoint2.setPaperInstance(this.paper);
-
-  var pathString = 'M' + this.anchorPoint1.position.x + ',' + this.anchorPoint1.position.y;
-  pathString += 'L' + this.anchorPoint2.position.x + ',' + this.anchorPoint2.position.y;
-  this.path = paper.path(pathString);
-  nonInteractableSet.push(this.path);
 
   this.setPaperInstance = function(paper) {
     this.paper = paper;
@@ -123,20 +123,39 @@ function AnchorLine(circle1, circle2, anchorSystem) {
     var p1 = new Point2D(this.circle1.circle.attr('cx'), this.circle1.circle.attr('cy'));
     var p2 = new Point2D(this.circle2.circle.attr('cx'), this.circle2.circle.attr('cy'));
     var anchors = this.computeAnchorPositions();
-    var deltaP = p2.clone().subtract(p1);
-    var deltaAnchor = anchors[1].clone().subtract(anchors[0]);
-    if ((deltaP.x * deltaAnchor.x >= 0) && (deltaP.y * deltaAnchor.y >= 0)) {
-      this.anchorPoint1.setPosition(anchors[0]);
-      this.anchorPoint2.setPosition(anchors[1]);
+    if (anchors.length !== 0) {
+      var deltaP = p2.clone().subtract(p1);
+      var deltaAnchor = anchors[1].clone().subtract(anchors[0]);
+      if ((deltaP.x * deltaAnchor.x >= 0) && (deltaP.y * deltaAnchor.y >= 0)) {
+        this.anchorPoint1.setPosition(anchors[0]);
+        this.anchorPoint2.setPosition(anchors[1]);
+      } else {
+        this.anchorPoint1.setPosition(anchors[1]);
+        this.anchorPoint2.setPosition(anchors[0]);
+      }
+
+      var pathString = 'M' + this.anchorPoint1.position.x + ',' + this.anchorPoint1.position.y;
+      pathString += 'L' + this.anchorPoint2.position.x + ',' + this.anchorPoint2.position.y;
+      this.path.attr('path', pathString);
+      this.path.show();
     } else {
-      this.anchorPoint1.setPosition(anchors[1]);
-      this.anchorPoint2.setPosition(anchors[0]);
+      this.path.hide();
     }
 
-    var pathString = 'M' + this.anchorPoint1.position.x + ',' + this.anchorPoint1.position.y;
-    pathString += 'L' + this.anchorPoint2.position.x + ',' + this.anchorPoint2.position.y;
-    this.path.attr('path', pathString);
+    this.anchorPoint1.update();
+    this.anchorPoint2.update();
   }
+
+  // just initialize the anchorPoints, they will be positioned correctly in the update right after
+  this.anchorPoint1 = new AnchorPoint(new Point2D(0, 0), this.circle1, this);
+  this.anchorPoint2 = new AnchorPoint(new Point2D(0, 0), this.circle2, this);
+  this.anchorPoint1.setPaperInstance(this.paper);
+  this.anchorPoint2.setPaperInstance(this.paper);
+
+  this.path = paper.path('M0,0L0,0');
+  nonInteractableSet.push(this.path);
+
+  this.update();
 }
 
 function AnchorPoint(point, circle, anchorLine) {
@@ -190,5 +209,13 @@ function AnchorPoint(point, circle, anchorLine) {
     this.position.setTo(pos);
     this.handle.attr('cx', this.position.x);
     this.handle.attr('cy', this.position.y);
+  }
+
+  this.update = function() {
+    if (!this.anchorLine.anchorSystem.isCloseToRegion(this.associatedCircle.point, this.associatedCircle.circle.attr('r'))) {
+      this.handle.hide();
+    } else {
+      this.handle.show();
+    }
   }
 }
